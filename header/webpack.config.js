462294fa7b6092ad75d2b77d5c54703ad564147c
Paul-HenryN <ngpaulhenry@gmail.com>
2025-03-03 13:32:33 +0100

    feat: add initial header microfrontend with React and Webpack configuration

---
+++ header/webpack.config.js
@@ -0,0 +1,50 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { ModuleFederationPlugin } = require("webpack").container;
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  output: {
+    publicPath: "http://localhost:3001/",
+  },
+  devServer: {
+    port: 3001,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react", "@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "header",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./Header": "./src/components/Header",
+      },
+      shared: {
+        react: { singleton: true, requiredVersion: "^18.2.0" },
+        "react-dom": { singleton: true, requiredVersion: "^18.2.0" },
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
